a8af7f4dd42a60e55817c5f4aa8d98ce310ceea3
Marcelo Pereira Barbosa <mpbarbosa@gmail.com>
2025-08-06 11:23:25 -0300

    renderizando localidades

--- andarilho.py
+++ andarilho.py
@@ -19,43 +19,52 @@ def api_url():
     return ibge_host() + "/" + api_path()
 
 
-def url_tipo_loc(tipo_loc):
-    return api_url() + "/" + tipo_loc
+def ibge_api_url(tipo_loc: str, loc_id: str | None = None):
+    url = api_url() + "/" + tipo_loc
+    if loc_id:
+        url = url + "/" + loc_id
+    return url
 
 
-def get_api_data(tipo_loc):
-    url = url_tipo_loc(tipo_loc)
+def get_api_data(tipo_loc: str, loc_id: str | None = None):
+    url = ibge_api_url(tipo_loc, loc_id)
     r = requests.get(url)
+    r.raise_for_status()
     return json.loads(r.content)
 
 
-def render_localidade(tipo_loc):
+def render_tipo_localidade(tipo_loc):
     ibge_data = get_api_data(tipo_loc)
     descr = descr_tipo_loc(tipo_loc)
     return render_template(
-        tipo_loc + ".html", ibge_data=ibge_data, descr_tipo_loc=descr
+        "body_locs.html", tipo_loc=tipo_loc, ibge_data=ibge_data, descr_tipo_loc=descr
     )
 
 
-def render_localidade2(tipo_loc):
-    ibge_data = get_api_data(tipo_loc)
+def render_localidade(tipo_loc, loc_id):
+    ibge_data = get_api_data(tipo_loc, loc_id)
     descr = descr_tipo_loc(tipo_loc)
-    return render_template("body_loc.html", ibge_data=ibge_data, descr_tipo_loc=descr)
+    return render_template(
+        "body_localidade.html",
+        tipo_loc=tipo_loc,
+        ibge_data=ibge_data,
+        descr_tipo_loc=descr,
+    )
 
 
 def tipos_loc():
     tipos = {
-        "aglomeracos_urbanas": "Aglomerações Urbanas",
+        "aglomeracoes-urbanas": "Aglomerações Urbanas",
         "distritos": "Distritos",
         "mesorregioes": "Mesorregiões",
-        "microrregios": "Microrregiões",
+        "microrregioes": "Microrregiões",
         "municipios": "Municípios",
         "paises": "Países",
         "regioes": "Regiões",
-        "regioesimediatas": "Regiões Imediatas",
-        "regioesintegradasdedesenvolvimento": "Regoões Integradas de Desenvolvimento",
-        "regioesintermediarias": "Regiões Intermediárias",
-        "regioesmetropolitanas": "Regiões Metropolitanas",
+        "regioes-imediatas": "Regiões Imediatas",
+        "regioes-integradas-de-desenvolvimento": "Regoões Integradas de Desenvolvimento",
+        "regioes-intermediarias": "Regiões Intermediárias",
+        "regioes-metropolitanas": "Regiões Metropolitanas",
         "subdistritos": "Subdistritos",
         "estados": "UFs",
     }
@@ -73,7 +82,12 @@ def home():
 
 @app.route("/localidades/<tipo_loc>")
 def tipo_loc(tipo_loc):
-    return render_localidade2(tipo_loc)
+    return render_tipo_localidade(tipo_loc)
+
+
+@app.route("/localidades/<tipo_loc>/<loc_id>")
+def loc(tipo_loc, loc_id):
+    return render_localidade(tipo_loc, loc_id)
 
 
 @app.route("/ufs")
